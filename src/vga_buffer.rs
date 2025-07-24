5e0ddcfaede28640cae5023c9bc42d7528b83cc8
Sean McLeaish <smcleaish@gmail.com>
2025-07-24 12:42:40 -0400

    Added volatile, write to buffer

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -1,3 +1,5 @@
+use volatile::Volatile;
+
 #[allow(dead_code)]
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 #[repr(u8)]
@@ -42,7 +44,7 @@ const BUFFER_WIDTH: usize = 80;
 
 #[repr(transparent)]
 struct Buffer {
-    chars: [[ScreenChar; BUFFER_WIDTH]; BUFFER_HEIGHT],
+    chars: [[Volatile<ScreenChar>; BUFFER_WIDTH]; BUFFER_HEIGHT],
 }
 
 pub struct Writer {
@@ -62,12 +64,33 @@ impl Writer {
                 let row = BUFFER_HEIGHT - 1;
                 let col = self.column_position;
                 let color_code = self.color_code;
-                self.buffer.chars[row][col] = ScreenChar {
+                self.buffer.chars[row][col].write(ScreenChar {
                     ascii_char: byte,
                     color_code,
-                };
+                });
                 self.column_position += 1;
             }
         }
     }
+    pub fn write_str(&mut self, s: &str) {
+        for byte in s.bytes() {
+            match byte {
+                0x20..0x7e | b'\n' => self.write_byte(byte),
+                _ => self.write_byte(0xfe),
+            }
+        }
+    }
+    fn new_line(&mut self) { /* TODO */
+    }
+}
+
+pub fn print_something() {
+    let mut writer = Writer {
+        column_position: 0,
+        color_code: ColorCode::new(Color::LightGreen, Color::Black),
+        buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
+    };
+    writer.write_byte(b'H');
+    writer.write_str("ello");
+    writer.write_str("Wörld!");
 }
