9ca7a1b21f2855e87629c184b9b48b3bd988c285
Sean McLeaish <smcleaish@gmail.com>
2025-07-28 19:39:11 -0500

    Finished println! macro

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,18 @@
 #![no_std]
 #![no_main]
-
+use core::panic::PanicInfo;
 use os::println;
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
-    println!("Hello World{}", "!");
+    println!("Test");
+    println!("Second line");
+    panic!("Some panic");
+    loop {}
+}
 
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
