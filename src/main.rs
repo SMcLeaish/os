85e163415eb7aaf231bf86455c034497284dc6da
Sean McLeaish <smcleaish@gmail.com>
2025-08-01 20:11:30 -0500

    Built through chapter 3

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
 #![no_std]
 #![no_main]
+
+mod vga_buffer;
+
 use core::panic::PanicInfo;
-use os::println;
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
     println!("Test");
     println!("Second line");
-    panic!("Some panic");
     loop {}
 }
 
